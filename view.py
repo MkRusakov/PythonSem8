a1302a6ba9995def42c890175f926587a87f4522
Mikhail <i@MkRusakov.ru>
2022-11-07 10:51:37 +0300

    end

---
+++ view.py
@@ -0,0 +1,55 @@
+def out_red(text):
+    print("\033[31m {}\033[0m" .format(text))
+
+def out_yellow(text):
+    print("\033[33m {}\033[0m" .format(text))
+
+
+def start_menu():
+    out_yellow('Выберите необходимое действие!')
+    print('1 - показать все')
+    print('2 - добавить запись')
+    print('3 - удалить запись')
+    print('4 - изменить телефон')
+    return int(input('Введите цифру: '))
+
+
+
+def add_info():  
+    out_yellow('Введите ФИО и тел через пробел')
+    in_info = input().split()
+    return in_info
+
+
+
+def delete():  
+    out_yellow('Введите индекс для удаления')
+    return int(input())
+
+
+def change_tel(): 
+    out_yellow('Введите индекс для изменения и новый телефон, через энтер')
+    return int(input()), input()
+
+
+
+def change_menu ():   
+    out_yellow('Выберите необходимое действие!')
+    out_red('Вы хотите: ')
+    print ('1 - сохранить изменеия в текущем файле')
+    print ('2 - экспортирвать в новый csv файл')
+    return int(input('введите цифру: '))
+
+
+def export_menu():   
+    out_yellow('Выберите необходимое действие!')
+    out_red('Вы хотите: ')
+    print ('1 - пересохранить файл в новый файл csv')
+    print ('2 - выйти')
+    return int(input('введите цифру: '))
+
+
+
+def show_file(file_csv): 
+    for i, row in enumerate(file_csv):
+        print(i, ' '.join(row))
